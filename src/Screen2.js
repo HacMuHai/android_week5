b1b3cef1614187094f59e648371ad255e7496700
Your Name <you@example.com>
2023-10-16 09:41:52 +0700

    commit screen1 screen2

---
+++ src/Screen2.js
@@ -0,0 +1,193 @@
+import React, { useState } from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
+
+
+
+// export default function Screen2({navigation}) {
+function Screen2({ navigation,route }) {
+  
+  const [pathImgPhone, setPathImgPhone] = useState(route.params)
+  console.log(pathImgPhone)
+  // const [mauChon,setMauChon] =  useState(require('../assets/vs_red.png'))
+  const onChangeMau = (vMau) => {
+    switch (vMau) {
+      case 'silver':
+        // pathImgPhone = require('../assets/vs_silver.png')
+        setPathImgPhone(require('../assets/vs_silver.png'))
+        break
+      case 'red':
+        setPathImgPhone(require('../assets/vs_red.png'))
+        // pathImgPhone = require('../assets/vs_red.png')
+        break
+      case 'blue':
+        setPathImgPhone(require('../assets/vs_blue.png'))
+        // pathImgPhone = require('../assets/vs_blue.png')
+        break
+      default:
+        setPathImgPhone(require('../assets/vs_black.png'))
+        // pathImgPhone = require('../assets/vs_black.png')
+        break
+    }
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerInfo}>
+        <View style={styles.boxImgPhone}>
+          <Image
+            // source={require('../assets/vs_black.png')}
+            source={pathImgPhone}
+            style={styles.imgPhone}
+          /></View>
+
+        <View style={styles.boxInfo}>
+          <Text style={styles.txtInfo}>Điện Thoại Vsmart Joy 3</Text>
+          <Text style={styles.txtInfo}>Hàng chính hãng </Text>
+        </View>
+      </View>
+
+      <View style={styles.containerBtnMau}>
+        <Text style={styles.txtChonMau}>Chọn một màu bên dưới:</Text>
+        <View style={styles.boxBtnMau}>
+          <TouchableOpacity
+            style={styles.btnMau}
+            onPress={()=>onChangeMau('silver')}
+          // value={'silver'}
+          >
+            <View style={styles.mauSilver}></View>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.btnMau}
+            onPress={()=>onChangeMau('red')}
+          // value={'red'}
+          >
+            <View style={styles.mauRed}></View>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.btnMau}
+            onPress={()=>onChangeMau('black')}
+          // value={'black'}
+          >
+            <View style={styles.mauBlack}></View>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.btnMau}
+            onPress={()=>onChangeMau('blue')}
+          // value={'blue'}
+          >
+            <View style={styles.mauBlue}></View>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.boxBtnXong}>
+          <TouchableOpacity style={styles.btnXong} onPress={() => navigation.navigate('Screen1',pathImgPhone)}>
+            <Text style={styles.txtXong}>Xong</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+
+  );
+}
+export default Screen2;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center"
+  },
+
+  containerInfo: {
+    flex: 2,
+    width: "95%",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    gap: 10
+
+  },
+  boxImgPhone: {
+    flex: 3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  imgPhone: {
+    width: 112,
+    height: 132,
+  },
+
+  boxInfo: {
+    flex: 7,
+    height: "60%",
+    justifyContent: "flex-start"
+  },
+  txtInfo: {
+    fontSize: 16,
+    fontWeight: 600
+  },
+
+  containerBtnMau: {
+    flex: 7,
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    backgroundColor: "#C4C4C4"
+  },
+  txtChonMau: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: 600,
+    margin: 15,
+  },
+
+  boxBtnMau: {
+    flex: 7,
+    width: "100%",
+    // flexDirection:"row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    gap: 10
+  },
+  btnMau: {
+    flex: 1
+  },
+  mauSilver: {
+    width: 85,
+    height: 80,
+    backgroundColor: "#C5F1FB",
+  },
+  mauRed: {
+    width: 85,
+    height: 80,
+    backgroundColor: "#F30D0D",
+  },
+  mauBlack: {
+    width: 85,
+    height: 80,
+    backgroundColor: "#000000",
+  },
+  mauBlue: {
+    width: 85,
+    height: 80,
+    backgroundColor: "#234896",
+  },
+  boxBtnXong: {
+    flex: 1.5,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  btnXong: {
+    width: "90%",
+    height: 45,
+    backgroundColor: "#1952E2",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderBlockColor: "#CA1536"
+  },
+
+});
---
+++ src/Screen3.js
@@ -0,0 +1,216 @@
+import React from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
+import Sreen2 from './Sreen2'
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.containerImg}>
+                <Image
+                    style={styles.imgPhone}
+                    source={require('../assets/vs_black.png')}
+                />
+            </View>
+            <View style={styles.containerInfo}>
+                <Text style={styles.txtName}>Điện Thoại Vsmart Joy 3 - Hàng chính hãng</Text>
+                <View style={styles.boxDanhGia}>
+                    <View style={styles.boxStar}>
+                        <Image
+                            style={styles.imgStar}
+                            source={require('../assets/star.png')}
+                        />
+                        <Image
+                            style={styles.imgStar}
+                            source={require('../assets/star.png')}
+                        />
+                        <Image
+                            style={styles.imgStar}
+                            source={require('../assets/star.png')}
+                        />
+                        <Image
+                            style={styles.imgStar}
+                            source={require('../assets/star.png')}
+                        />
+                        <Image
+                            style={styles.imgStar}
+                            source={require('../assets/star.png')}
+                        />
+                    </View>
+                    <Text style={styles.txtSLDanhGia}>(Xem 828 đánh giá)</Text>
+                </View>
+                <View style={styles.boxPrice}>
+                    <Text style={styles.txtGiaBan}>1.790.000 đ</Text>
+                    <Text style={styles.txtGiaGoc}>1.990.000 đ</Text>
+                </View>
+                <View style={styles.boxHoanTien}>
+                    <Text style={styles.txtHoanTien}>Ở ĐÂU RẺ HƠN HOÀN TIỀN</Text>
+                    <TouchableOpacity style={styles.btnChamHoi}>
+                        <Image
+                            style={styles.imgChamHoi}
+                            source={require('../assets/chamHoi.png')}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <View style={styles.containerBtn}>
+                <TouchableOpacity  style={styles.btnChuyenMau} onPress={() => navigation.navigate('Details')}>
+                    <Text style={styles.txtChuyenMau}>4 MÀU-CHỌN MÀU</Text>
+                    <Image
+                            style={styles.imgPass}
+                            source={require('../assets/pass.png')}
+                        />
+                </TouchableOpacity>
+                <TouchableOpacity  style={styles.btnChonMua}>
+                    <Text style={styles.txtChonMua}>CHỌN MUA</Text> 
+                </TouchableOpacity>
+            </View>
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    //Img phone
+    containerImg: {
+        flex: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imgPhone: {
+        width: "100%",
+        height: "100%",
+        resizeMode: 'contain'
+    },
+
+
+    //Info
+    containerInfo: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // width:"90%"
+        gap:15
+    },
+    txtName:{
+        flex:1,
+        width:"90%",
+        fontSize:16,
+        fontWeight:400
+    },
+    boxDanhGia:{
+        flex:1,
+        width:"90%",
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        gap:20
+    },
+    boxStar:{
+        flex:4,
+        height:50,
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center'
+    },
+
+    imgStar: {
+        width: 28,
+        height: 28,
+        resizeMode: 'contain'
+    },
+
+    txtSLDanhGia:{
+        flex:6,
+        fontSize:16,
+        fontWeight:400
+    },
+
+    boxPrice:{
+        flex:1,
+        width:"90%",
+        flexDirection:'row',
+        justifyContent:'flex-start',
+        alignItems:'center',
+        gap:30
+    },
+
+    txtGiaBan:{
+        // width:"30%",
+        fontSize:24,
+        fontWeight:700
+    },
+    txtGiaGoc:{
+        // width:"30%",
+        fontSize:16,
+        fontWeight:500,
+        textDecorationLine:'line-through'
+    },
+
+    boxHoanTien:{
+        flex:1,
+        width:"90%",
+        flexDirection:'row',
+        justifyContent:'flex-start',
+        alignItems:'center',
+        gap:15
+    },
+    txtHoanTien:{
+        fontSize:14,
+        fontWeight:700,
+        color:"red"
+    },
+    imgChamHoi: {
+        width: 20,
+        height: 20,
+        resizeMode: 'contain'
+    },
+
+    //button
+    containerBtn: {
+        position:"relative",
+        flex: 2,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        // width:"90%",
+    },
+    btnChuyenMau:{
+        // flex:4,
+        height:40,
+        width:"90%",
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:10,
+        borderWidth:1,
+    },
+    txtChuyenMau:{
+        fontSize:16,
+        fontWeight:500,
+    },
+    imgPass: {
+        position:"absolute",
+        right:10,
+        width: 15,
+        height: 20,
+        resizeMode: 'contain'
+    },
+    btnChonMua:{
+        // flex:4,
+        height:40,
+        width:"90%",
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:10,
+        backgroundColor:"red"
+    },
+    txtChonMua:{
+        fontSize:18,
+        fontWeight:600,
+        color:"white"
+    }
+});
